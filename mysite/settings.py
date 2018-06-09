40443e99efe4fd10a014bd142352ade3f8a36226
catalina <catalina0902@gmail.com>
2018-06-09 15:32:34 -0500

    correccion

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fe$_@w$xt%m9%!r5og6xz^0eifjqegm-(dz%t*#u8uij05y+mb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1', '<tu-usuario>.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "catalinaagredo.pythonanywhere.com","elprimerblog.pythonanywhere.com"]
 
 
 # Application definition
